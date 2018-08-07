714f0f30b18c6d422d920cdd035c4795e863f2d6
Musa Jahun <msjahun@live.com>
2018-08-07 21:33:16 +0300

    didn't change anything

--- Kibrisorder_Email_generator/Models/Product.cs
+++ Kibrisorder_Email_generator/Models/Product.cs
@@ -8,12 +8,12 @@ namespace Kibrisorder_Email_generator.Models
 {
     public class Product { 
     
-        public String id { get; set; }
-        public String name { get; set; }
-        public String price { get; set; }
-        public String currency { get; set; }
-        public String img_url { get; set; }
-        public String product_url { get; set; }
+        public String id            { get; set; }
+        public String name          { get; set; }
+        public String price         { get; set; }
+        public String currency       { get; set; }
+        public String img_url       { get; set; }
+        public String product_url    { get; set; }
        
        
 
