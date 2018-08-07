7f1617e57a1d08168e8532d05e73db828f7a8986
Musa Jahun <msjahun@live.com>
2018-08-07 18:23:02 +0300

    added the product class

---
+++ Kibrisorder_Email_generator/Models/Product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Xml.Linq;
+
+namespace Kibrisorder_Email_generator.Models
+{
+    public class Product { 
+    
+        public String id { get; set; }
+        public String name { get; set; }
+        public String price { get; set; }
+        public String currency { get; set; }
+        public String img_url { get; set; }
+        public String product_url { get; set; }
+       
+       
+
+    }
+
+
+}
