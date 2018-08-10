685ec738214aed1916bc2bd63a7719a394036215
Musa Jahun <msjahun@live.com>
2018-08-10 19:52:16 +0300

    changes I've made

--- Kibrisorder_Email_generator/Controllers/EmailGeneratorController.cs
+++ Kibrisorder_Email_generator/Controllers/EmailGeneratorController.cs
@@ -14,51 +14,54 @@ namespace Kibrisorder_Email_generator.Controllers
     {
         // GET: EmailGenerator
         public ActionResult Index()
+        { return View("Index"); }
+
+        public ActionResult Generator()
         {
-           
 
-                ViewBag.section_header_text = Resources.product_recommendation_data_supermarket. section_header_text;
-            ViewBag.section_header_main_text = Resources.product_recommendation_data_supermarket. section_header_main_text;
 
-            ViewBag.product_1_image_url = Resources.product_recommendation_data_supermarket. product_1_image_url;
-            ViewBag.product_1_product_name = Resources.product_recommendation_data_supermarket. product_1_product_name;
-            ViewBag.product_1_price = Resources.product_recommendation_data_supermarket. product_1_price;
-            ViewBag.product_1_currency_symbol = Resources.product_recommendation_data_supermarket. product_1_currency_symbol;
-            ViewBag.product_1_buy_now_link = Resources.product_recommendation_data_supermarket. product_1_buy_now_link;
+            ViewBag.section_header_text = Resources.product_recommendation_data_supermarket.section_header_text;
+            ViewBag.section_header_main_text = Resources.product_recommendation_data_supermarket.section_header_main_text;
 
-            ViewBag.product_2_image_url = Resources.product_recommendation_data_supermarket. product_2_image_url;
-            ViewBag.product_2_product_name = Resources.product_recommendation_data_supermarket. product_2_product_name;
-            ViewBag.product_2_price = Resources.product_recommendation_data_supermarket. product_2_price;
-            ViewBag.product_2_currency_symbol = Resources.product_recommendation_data_supermarket. product_2_currency_symbol;
-            ViewBag.product_2_buy_now_link = Resources.product_recommendation_data_supermarket. product_2_buy_now_link;
+            ViewBag.product_1_image_url = Resources.product_recommendation_data_supermarket.product_1_image_url;
+            ViewBag.product_1_product_name = Resources.product_recommendation_data_supermarket.product_1_product_name;
+            ViewBag.product_1_price = Resources.product_recommendation_data_supermarket.product_1_price;
+            ViewBag.product_1_currency_symbol = Resources.product_recommendation_data_supermarket.product_1_currency_symbol;
+            ViewBag.product_1_buy_now_link = Resources.product_recommendation_data_supermarket.product_1_buy_now_link;
 
-            ViewBag.product_3_image_url = Resources.product_recommendation_data_supermarket. product_3_image_url;
-            ViewBag.product_3_product_name = Resources.product_recommendation_data_supermarket. product_3_product_name;
-            ViewBag.product_3_price = Resources.product_recommendation_data_supermarket. product_3_price;
-            ViewBag.product_3_currency_symbol = Resources.product_recommendation_data_supermarket. product_3_currency_symbol;
-            ViewBag.product_3_buy_now_link = Resources.product_recommendation_data_supermarket. product_3_buy_now_link;
+            ViewBag.product_2_image_url = Resources.product_recommendation_data_supermarket.product_2_image_url;
+            ViewBag.product_2_product_name = Resources.product_recommendation_data_supermarket.product_2_product_name;
+            ViewBag.product_2_price = Resources.product_recommendation_data_supermarket.product_2_price;
+            ViewBag.product_2_currency_symbol = Resources.product_recommendation_data_supermarket.product_2_currency_symbol;
+            ViewBag.product_2_buy_now_link = Resources.product_recommendation_data_supermarket.product_2_buy_now_link;
 
-            ViewBag.product_4_image_url = Resources.product_recommendation_data_supermarket. product_4_image_url;
-            ViewBag.product_4_product_name = Resources.product_recommendation_data_supermarket. product_4_product_name;
-            ViewBag.product_4_price = Resources.product_recommendation_data_supermarket. product_4_price;
-            ViewBag.product_4_currency_symbol = Resources.product_recommendation_data_supermarket. product_4_currency_symbol;
-            ViewBag.product_4_buy_now_link = Resources.product_recommendation_data_supermarket. product_4_buy_now_link;
+            ViewBag.product_3_image_url = Resources.product_recommendation_data_supermarket.product_3_image_url;
+            ViewBag.product_3_product_name = Resources.product_recommendation_data_supermarket.product_3_product_name;
+            ViewBag.product_3_price = Resources.product_recommendation_data_supermarket.product_3_price;
+            ViewBag.product_3_currency_symbol = Resources.product_recommendation_data_supermarket.product_3_currency_symbol;
+            ViewBag.product_3_buy_now_link = Resources.product_recommendation_data_supermarket.product_3_buy_now_link;
 
-            ViewBag.product_5_image_url = Resources.product_recommendation_data_supermarket. product_5_image_url;
-            ViewBag.product_5_product_name = Resources.product_recommendation_data_supermarket. product_5_product_name;
-            ViewBag.product_5_price = Resources.product_recommendation_data_supermarket. product_5_price;
-            ViewBag.product_5_currency_symbol = Resources.product_recommendation_data_supermarket. product_5_currency_symbol;
-            ViewBag.product_5_buy_now_link = Resources.product_recommendation_data_supermarket. product_5_buy_now_link;
+            ViewBag.product_4_image_url = Resources.product_recommendation_data_supermarket.product_4_image_url;
+            ViewBag.product_4_product_name = Resources.product_recommendation_data_supermarket.product_4_product_name;
+            ViewBag.product_4_price = Resources.product_recommendation_data_supermarket.product_4_price;
+            ViewBag.product_4_currency_symbol = Resources.product_recommendation_data_supermarket.product_4_currency_symbol;
+            ViewBag.product_4_buy_now_link = Resources.product_recommendation_data_supermarket.product_4_buy_now_link;
 
-            ViewBag.product_6_image_url = Resources.product_recommendation_data_supermarket. product_6_image_url;
-            ViewBag.product_6_product_name = Resources.product_recommendation_data_supermarket. product_6_product_name;
-            ViewBag.product_6_price = Resources.product_recommendation_data_supermarket. product_6_price;
-            ViewBag.product_6_currency_symbol = Resources.product_recommendation_data_supermarket. product_6_currency_symbol;
-            ViewBag.product_6_buy_now_link = Resources.product_recommendation_data_supermarket. product_6_buy_now_link;
+            ViewBag.product_5_image_url = Resources.product_recommendation_data_supermarket.product_5_image_url;
+            ViewBag.product_5_product_name = Resources.product_recommendation_data_supermarket.product_5_product_name;
+            ViewBag.product_5_price = Resources.product_recommendation_data_supermarket.product_5_price;
+            ViewBag.product_5_currency_symbol = Resources.product_recommendation_data_supermarket.product_5_currency_symbol;
+            ViewBag.product_5_buy_now_link = Resources.product_recommendation_data_supermarket.product_5_buy_now_link;
+
+            ViewBag.product_6_image_url = Resources.product_recommendation_data_supermarket.product_6_image_url;
+            ViewBag.product_6_product_name = Resources.product_recommendation_data_supermarket.product_6_product_name;
+            ViewBag.product_6_price = Resources.product_recommendation_data_supermarket.product_6_price;
+            ViewBag.product_6_currency_symbol = Resources.product_recommendation_data_supermarket.product_6_currency_symbol;
+            ViewBag.product_6_buy_now_link = Resources.product_recommendation_data_supermarket.product_6_buy_now_link;
 
             ViewBag.shared_buy_now_button = Resources.product_recommendation_data_supermarket.shared_buy_now_button;
 
-            String xmlDocPath = Server.MapPath("~/Content/recommended_products.xml"); 
+            String xmlDocPath = Server.MapPath("~/Content/recommended_products.xml");
 
             XDocument xdoc = XDocument.Load(xmlDocPath);
             productXMLinterface context = new productXMLinterface(xdoc, xmlDocPath);
@@ -67,9 +70,11 @@ namespace Kibrisorder_Email_generator.Controllers
             List<Product> products = new List<Product>();
             products = context.GetAllProducts();
 
-           
-        
+
+
             return View("View", products);
         }
+
+
     }
 }
